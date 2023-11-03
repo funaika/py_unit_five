9ac8c04a2bb843873089af58b164c8e4b1d567a0
funaika <funaika@ssfs.org>
2023-11-02 22:03:59 -0400

    :)

--- average.py
+++ average.py
@@ -2,7 +2,15 @@
 #kalen funai
 #11/02/2023
 
+numbers = []
 while True:
     userInput = input("input an integer or enter q to quit")
     if userInput == 'q':
         break
+
+    number = float(userInput)
+    numbers.append(number)
+
+
+sumNumbers = sum(numbers)
+print("The sum of the values input is", sumNumbers)
